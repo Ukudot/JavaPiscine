50e27d5fbd579eb78052292399c1e401512e0bb0
Giovanni Panico <gpanico@e4r9p4.42roma.it>
2023-07-03 10:48:02 +0200

    ex00 errata corrige

--- JAVA_00/ex00/Program.java
+++ JAVA_00/ex00/Program.java
@@ -7,11 +7,18 @@ class	Program
 
 		num = 479598;
 		sum = 0;
-		for (int i = 0; i < 6; i++)
-		{
-			sum += num % 10;
-			num /= 10;
-		}
+		sum += num % 10;
+		num /= 10;
+		sum += num % 10;
+		num /= 10;
+		sum += num % 10;
+		num /= 10;
+		sum += num % 10;
+		num /= 10;
+		sum += num % 10;
+		num /= 10;
+		sum += num % 10;
+		num /= 10;
 		System.out.println("Result: " + sum);
 	}
 }
