5f87732ed9f9f0daa4a8360668392772f5edf379
Giovanni Panico <gpanico@e4r9p4.42roma.it>
2023-07-14 11:00:43 +0200

    JAVA_04 ex00 completed

---
+++ JAVA_04/ex00/ImagesToChar/README.txt
@@ -0,0 +1,16 @@
+* DESCRIPTION *
+This is a simple program to print with ASCII a BMP black and white image
+
+* IMPORTANT *
+The only files accepted are black and white binary BMP
+
+* COMPILATION *
+javac -d ./target/edu.42Roma.printer src/edu.42Roma.printer/app/App.java src/edu.42Roma.printer/logic/*.java
+
+* EXECUTION *
+java -cp ./target/edu.42Roma.printer App <white_char> <black_char> <image_path>
+
+* INFO PARAMS *
+white_char: Character used to represent white color
+black_char: Character used to represent black color
+image_path:  path to the image
--- JAVA_04/ex00/ImagesToChar/src/edu.42Roma.printer/app/App.java
+++ JAVA_04/ex00/ImagesToChar/src/edu.42Roma.printer/app/App.java
@@ -1,12 +1,28 @@
 class	App {
-	public static void main(String args[]) {
-		ImageConverter	imageConverter;
 
+	private static Image	takeInput(String args[]) throws InvalidInputException {
+		Image	image = null;
+
+		if (args[0].length() != 1 || args[1].length() != 1) {
+			throw new InvalidInputException("Invalid input");
+		}
+		image = new Image(args[0].charAt(0), args[1].charAt(0), args[2]);
+		return (image);
+	}
+
+	public static void main(String args[]) {
+		Image	image = null;
+
+		if (args.length != 3) {
+			System.err.println("Usage error: java App <white_char> <black_char> <image_path>");
+			System.exit(-1);
+		}
 		try {
-			imageConvertert = takeInput(args);
+			image = takeInput(args);
 		} catch (InvalidInputException e) {
 			System.err.println(e);
 			System.exit(-1);
 		}
+		image.print();
 	}
 }
