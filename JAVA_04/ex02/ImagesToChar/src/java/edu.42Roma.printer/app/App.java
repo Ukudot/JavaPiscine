93295ffb2d02137053435afeb8e6ab2faf1d1903
Ukudot <giovannipanico71@yahoo.it>
2023-07-15 09:51:33 +0200

    JAVA_04 ex02 reset

--- JAVA_04/ex02/ImagesToChar/src/java/edu.42Roma.printer/app/App.java
+++ JAVA_04/ex02/ImagesToChar/src/java/edu.42Roma.printer/app/App.java
@@ -1,54 +1,33 @@
-import	com.beust.jcommander.Parameter;
-import	com.beust.jcommander.Parameters;
-import	com.beust.jcommander.JCommander;
-import	com.beust.jcommander.IParameterValidator;
-import	com.beust.jcommander.ParameterException;
+package	app;
+import	logic.*;
 
-@Parameters(separators = "=")
-class	App {
-	@Parameter(names = "-white", validateWith = SingleChar.class)
-	private String	white;
-	@Parameter(names = "-black", validateWith = SingleChar.class)
-	private String	black;
+public class	App {
 
-	private static final String	FILE = "../resources/it.bmp";
+	private static final String	FILE = "./target/resources/it.bmp";
+
+	private static Image	takeInput(String args[]) throws InvalidInputException {
+		Image	image = null;
+
+		if (args[0].length() != 1 || args[1].length() != 1) {
+			throw new InvalidInputException("Invalid input");
+		}
+		image = new Image(args[0].charAt(0), args[1].charAt(0), FILE);
+		return (image);
+	}
 
 	public static void main(String args[]) {
-		App	app = new App();
+		Image	image = null;
 
 		if (args.length != 2) {
 			System.err.println("Usage error: java App <white_char> <black_char>");
 			System.exit(-1);
 		}
 		try {
-			JCommander.newBuilder()
-				.addObject(app)
-				.build()
-				.parse(args);
-			app.run();
-		} catch (ParameterException e) {
-			System.err.println(e);
-		}
-	}
-
-	public void	run() {
-		Image	image = null;
-
-		try {
-			image = new Image(this.white.charAt(0), this.black.charAt(0), FILE);
+			image = takeInput(args);
 		} catch (InvalidInputException e) {
 			System.err.println(e);
 			System.exit(-1);
 		}
 		image.print();
-	}
-
-	public static class SingleChar implements IParameterValidator {
-		public	SingleChar() {}
-		public void validate(String name, String value) throws ParameterException {
-			if (value == null || value.length() != 1) {
-				throw new ParameterException("Invalid input");
-			}
-		}
 	}
 }
