79893bb89563c5bf575b2b4456d26654533ac68f
Ukudot <giovannipanico71@yahoo.it>
2023-07-24 19:19:15 +0200

    JAVA_07 ex00 completed (Worst code written here)

--- JAVA_07/ex00/Reflection/src/main/java/edu/Roma42/app/App.java
+++ JAVA_07/ex00/Reflection/src/main/java/edu/Roma42/app/App.java
@@ -13,6 +13,7 @@ public class App
 {
 	private final static String	separator = "-----------------------";
 	private final static String	pack = "edu.Roma42.classes.";
+	private final static String	langPack = "java.lang.";
 
 	private static Class	selectClass(Scanner scanner) throws ClassNotFoundException {
 		String	line;
@@ -34,6 +35,7 @@ public class App
 	private static void	printMethods(Class selected) {
 		Method			methods[];
 		StringJoiner	params;
+		int				i = 0;
 
 		methods = selected.getDeclaredMethods();
 		for (Method method : methods) {
@@ -48,7 +50,8 @@ public class App
 		}
 	}
 
-	private static Object	createObject(Class selected, Scanner scanner) throws NoSuchMethodException, InstantiationException, IllegalAccessException, java.lang.reflect.InvocationTargetException {
+	private static Object	createObject(Class selected, Scanner scanner) throws NoSuchMethodException,
+			InstantiationException, IllegalAccessException, InvocationTargetException {
 		Constructor	constructors[];
 		Object		arglist[];
 		Parameter	params[];
@@ -78,10 +81,70 @@ public class App
 		return (created);
 	}
 
-	public static void main(String[] args)
+	public static Object	updateObject(Class selected, Object created, Scanner scanner) throws NoSuchFieldException, 
+		   InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+		String	line;
+		Field	field;
+
+		System.out.println("Enter name of the field for changing:");
+		System.out.print("-> ");
+		line = scanner.nextLine();
+		field = selected.getDeclaredField(line);
+		System.out.println("Enter " + field.getType().getSimpleName() + " value");
+		System.out.print("-> ");
+		line = scanner.nextLine();
+		field.setAccessible(true);
+		field.set(created, field.getType().getConstructor(String.class).newInstance(line));
+		System.out.println("Object updated: " + created);
+		return (created);
+	}
+
+	public static void	callMethod(Class selected, Object created, Scanner scanner) throws ClassNotFoundException,
+		   NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
+		String	line;
+		Method	method;
+		Class	params[];
+		String	types[];
+		Object	arglist[] = null;
+		Object	returnValue;
+
+		System.out.println("Enter name of the method for call:");
+		System.out.print("-> ");
+		line = scanner.nextLine();
+		types = line.substring(line.indexOf("(") + 1, line.length() - 1).split(",");
+		if (!types[0].trim().equals("")) {
+			params = new Class[types.length];
+			for (int i = 0; i < params.length; i++) {
+				params[i] = Class.forName(langPack + types[i].trim());
+			}
+		} else {
+			params = new Class[0];
+		}
+		method = selected.getDeclaredMethod(line.substring(0, line.indexOf("(")), params);
+		arglist = new Object[params.length];
+		for (int i = 0; i < params.length; i++) {
+			System.out.println("Enter " + params[i].getSimpleName() + " value");
+			System.out.print("-> ");
+			line = scanner.nextLine();
+			arglist[i] = params[i].getConstructor(String.class).newInstance(line);
+		}
+		if (arglist == null) {
+			returnValue = method.invoke(created);
+		} else {
+			returnValue = method.invoke(created, arglist);
+		}
+		if (method.getReturnType().equals(Class.forName(langPack + "Void"))) {
+			return ;
+		}
+		System.out.println("Method returned:");
+		System.out.println(returnValue);
+	}
+
+	public static void	main(String[] args)
 	{
 		Class	selected;
 		Scanner	scanner;
+		Object	created;
 
 		System.out.println("Classes:\nStudent\nUniveristy");
 		System.out.println(separator);
@@ -94,7 +157,11 @@ public class App
 			System.out.println("methods: ");
 			printMethods(selected);
 			System.out.println(separator);
-			createObject(selected, scanner);
+			created = createObject(selected, scanner);
+			System.out.println(separator);
+			created = updateObject(selected, created, scanner);
+			System.out.println(separator);
+			callMethod(selected, created, scanner);
 			scanner.close();
 		} catch (Exception e) {
 			System.out.println(e);
