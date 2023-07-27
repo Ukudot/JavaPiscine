1a23e439388ded76a3381d3c5e93abff945e4758
Giovanni Panico <gpanico@e3r3p3.42roma.it>
2023-07-27 11:27:32 +0200

    JAVA_08 ex00 testing time

--- JAVA_08/ex00/Spring/src/main/java/edu/Roma42/app/App.java
+++ JAVA_08/ex00/Spring/src/main/java/edu/Roma42/app/App.java
@@ -2,13 +2,14 @@ package	edu.Roma42.app;
 import	edu.Roma42.printer.*;
 import	edu.Roma42.renderer.*;
 import	edu.Roma42.preProcessor.*;
+import	org.springframework.context.ApplicationContext;
+import	org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class	App {
 	public static void	main(String args[]) {
-		PreProcessor preProcessor = new PreProcessorToUpperImpl();
-		Renderer renderer = new RendererErrImpl(preProcessor);
-		PrinterWithPrefixImpl printer = new PrinterWithPrefixImpl(renderer);
-		printer.setPrefix ("Prefix ");
-		printer.print ("Hello!");
+		ApplicationContext	context = new ClassPathXmlApplicationContext("context.xml");
+		Printer	printer = context.getBean("printerWithPrefix", Printer.class);
+		printer.setPrefix("Prefix");
+		printer.print("Hello!");
 	}
 }
